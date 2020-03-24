c1aacd9d22db8e610f8b2b61a4fffb6d6699e325
krisztinamat <41524209+krisztinamat@users.noreply.github.com>
2020-03-24 19:19:46 -0400

    edited sound class

--- soundElem.js
+++ soundElem.js
@@ -11,7 +11,7 @@ function generate(){
 
     var arrayPossible = ["C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
     "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
-    "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5"]
+    "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5"]
     
     var notesArray = [];
     
@@ -72,7 +72,7 @@ function getSound1(notesArray, btn, btn2){
     btn.disabled=true;
     btn2.disabled = true;
     
-    Tone.Transport.bpm.value = 150;
+    Tone.Transport.bpm.value = 200;
     
     toneArray = [];
     
