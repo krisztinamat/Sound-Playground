c05d52c5342c30cdaea726c4b5d8cf7e9257ca19
krisztinamat <41524209+krisztinamat@users.noreply.github.com>
2020-04-10 13:01:45 -0400

    extended treble range note id

--- randomizer.js
+++ randomizer.js
@@ -97,8 +97,8 @@ stave.addClef("treble");
 stave.setContext(context).draw();
 
 
-var noteBank = ["c/4", "d/4", "e/4", "f/4", "g/4", "a/4", "b/4", "c/5", "d/5",
-                "e/5", "f/5", "g/5", "a/5"]; 
+var noteBank = ["e/3", "f/3", "g/3", "a/3", "b/3", "c/4", "d/4", "e/4", "f/4", "g/4", "a/4", "b/4", "c/5", "d/5",
+                "e/5", "f/5", "g/5", "a/5", "b/5", "c/6"]; 
 
 var ans = "";
 
