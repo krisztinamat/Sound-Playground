7615dd4a1ab447c02647cfb62cdcfca1d9474510
krisztinamat <41524209+krisztinamat@users.noreply.github.com>
2020-04-23 11:26:21 -0400

    change two files into a single page: lil ear trainer

--- directiontrainer.js
+++ directiontrainer.js
@@ -10,9 +10,9 @@ window.onload = function() {
 
 function generate(){
 
-    var arrayPossible = ["C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
+    var arrayPossible = ["A2","A#2", "B2", "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
     "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
-    "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5"]
+    "C5", "C#5", "D5", "D#5", "E5"]
     
     var notesArray = [];
     
